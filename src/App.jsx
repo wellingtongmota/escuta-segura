54c4ccfe10b299b0a3150f62a5e51c1a6dd95d31
Wellington Galvao <wellingtongalvao96@gmail.com>
2023-05-28 16:29:10 -0300

    Instalação fontsource

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Flex, FormControl, FormLabel, Input, Text } from "@chakra-ui/react"
+import { Flex, FormControl, FormLabel, Heading, Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import '@fontsource/teko';
 
 function App() {
   const [name, setName] = useState('Opcional');
@@ -11,8 +12,8 @@ function App() {
       <Flex
         direction="column"
         align="center"
-        justify="center"
-        bgColor="gray.50"
+        justify="flex-start"
+        bgColor="gray.100"
         w='100%'
         h='100vh'
       >
@@ -24,6 +25,16 @@ function App() {
           gap={5}
           p={2}
         >
+
+          <Flex align="baseline" gap={2}>
+            <Heading as='h1' size='4xl' my={4} color="blue.700">
+              DOVALE
+            </Heading>
+            <Heading as="b" fontSize="3xl" color="orange.500">
+              Escuta Segura
+            </Heading>
+          </Flex>
+
           <Text
             fontSize={['sm', 'md', 'lg', 'xl']}
             textAlign="center"
@@ -34,7 +45,7 @@ function App() {
           <Flex minWidth='25em' maxWidth='lg' direction="column">
             <FormControl>
               <FormLabel>Nome: {name} </FormLabel>
-              <Input type='text' variant='outline' bgColor='white' onChange={handleName}/>
+              <Input type='text' variant='outline' bgColor='white' onChange={handleName} />
             </FormControl>
           </Flex>
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import theme from './utils/theme.js'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>,
 )
